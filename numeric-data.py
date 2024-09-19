b05cee55316a40fae2c1c9510d645ff344399648
Viviane Vida <67081266+VivianeVida@users.noreply.github.com>
2024-09-18 21:21:12 -0300

    Create numeric-data.py

---
+++ numeric-data.py
@@ -0,0 +1,24 @@
+2 + 2
+4 - 2
+2 * 2
+2 * 2
+4 / 2
+print("Python has three numeric types: int, float, and complex")
+myValue = 1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue = 3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue = 1 + 2j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue = True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
